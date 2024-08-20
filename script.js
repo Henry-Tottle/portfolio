f09b1168ad81fca4a3114befae8a1a32266b10d8
Henry Tottle <henrywilliamtottle@gmail.com>
2024-08-20 16:48:37 +0100

    responsive development

--- script.js
+++ script.js
@@ -4,16 +4,39 @@ const projectList2 = document.querySelector('.projectP2')
 const projectList3 = document.querySelector('.projectP3')
 const projectList4 = document.querySelector('.projectP4')
 
-projectCard.addEventListener('mouseenter', () => {
+const biogCard = document.querySelector('.biog')
+const blogList1 = document.querySelector('.blogP1')
+const blogList2 = document.querySelector('.blogP2')
+const blogList3 = document.querySelector('.blogP3')
+const blogList4 = document.querySelector('.blogP4')
+
+const removeHiddenProject = () => {
     projectList1.classList.remove('hidden')
     projectList2.classList.remove('hidden')
     projectList3.classList.remove('hidden')
     projectList4.classList.remove('hidden')
-})
-projectCard.addEventListener('mouseleave', () => {
+}
+const removeHiddenBiog = () => {
+    blogList1.classList.remove('hidden')
+    blogList2.classList.remove('hidden')
+    blogList3.classList.remove('hidden')
+    blogList4.classList.remove('hidden')
+}
+const addHiddenBiog = () => {
+    blogList1.classList.add('hidden')
+    blogList2.classList.add('hidden')
+    blogList3.classList.add('hidden')
+    blogList4.classList.add('hidden')
+}
+const addHidden = () => {
     projectList1.classList.add('hidden')
     projectList2.classList.add('hidden')
     projectList3.classList.add('hidden')
     projectList4.classList.add('hidden')
+}
 
-})
+projectCard.addEventListener('mouseenter', removeHiddenProject)
+projectCard.addEventListener('mouseleave', addHidden)
+
+biogCard.addEventListener('mouseenter', removeHiddenBiog)
+biogCard.addEventListener('mouseleave', addHiddenBiog)
